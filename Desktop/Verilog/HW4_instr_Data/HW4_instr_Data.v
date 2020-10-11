2b199b437adaf5a8168578f0715809b9939c3696
vmazashvili <vanomazashvili@gmail.com>
2020-10-11 23:20:18 +0400

    initial commit

---
+++ Desktop/Verilog/HW4_instr_Data/HW4_instr_Data.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:01:24 10/11/2020 
+// Design Name: 
+// Module Name:    HW4_instr_Data 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module HW4_instr_Data(
+    );
+
+
+endmodule
